e03cc4ab9c20a43349165ced8579c7a42619d1c8
Alejandro Canon Munoz <al3jandrocan0n@gmail.com>
2026-05-18 18:33:45 -0500

    Feature: Working in filters

--- src/components/Recipes/Filters/CustomFilters.tsx
+++ src/components/Recipes/Filters/CustomFilters.tsx
@@ -3,7 +3,7 @@ import searchImg from "../../../assets/images/icon-search.svg";
 
 const CustomFilters = () => {
   return (
-    <section className="flex flex-wrap justify-between xl:gap-4 gap-3 container">
+    <section className="flex flex-wrap justify-between xl:gap-4 gap-3 md:px-9 xl:px-18">
       <div className="flex flex-col md:flex-row gap-4 w-full md:w-auto">
         <button className="flex cursor-pointer gap-2 justify-center items-center bg-neutral-0 text-neutral-900 font-medium px-4 py-2.5 rounded-[10px] border-neutral-300 border">
           Max Prep Time{" "}
--- src/components/Recipes/RecipesPage.tsx
+++ src/components/Recipes/RecipesPage.tsx
@@ -5,26 +5,29 @@ import FoodCard from "./FoodCard/FoodCard";
 const RecipesPage = () => {
   return (
     <>
-      <div className="flex flex-col justify-center items-center gap-3">
+      <section className="flex flex-col justify-center items-center gap-3">
         <h1 className="text-neutral-900 text-[40px]/[1.2] md:text-[52px]/[1.2] xl:text-[48px]/[1.2] tracking-[-2px] font-extrabold">
           Explore our simple, healthy recipes
         </h1>
-        <p className="text-[20px] leading-[140%] tracking-[-0.5px] max-w-176 xl:max-w-180 md:text-center">
+        <p className="text-[20px] leading-[140%] tracking-[-0.5px] max-w-176 xl:max-w-180 md:text-center pb-12 xl:pb-16 ">
           Discover eight quick, whole-food dishes that fit real-life schedules
           and taste amazing. Use the search bar to find a recipe by name or
           ingredient, or simply scroll the list and let something delicious
           catch your eye.
         </p>
-        <CustomFilters />
-        <section className="flex flex-wrap  justify-center items-center gap-8">
-          {recipesData.map((recipe) => (
-            <FoodCard
-              key={recipe.id}
-              recipe={recipe}
-            />
-          ))}
-        </section>
-      </div>
+        <div className="flex flex-col gap-6">
+          <hr className="text-neutral-300" />
+          <CustomFilters />
+          <div className="flex flex-wrap justify-center items-center gap-8">
+            {recipesData.map((recipe) => (
+              <FoodCard
+                key={recipe.id}
+                recipe={recipe}
+              />
+            ))}
+          </div>
+        </div>
+      </section>
     </>
   );
 };
