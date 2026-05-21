60e3d1a93ddf40b1ca502d9e44c649fd90b4a498
Alejandro Canon Munoz <al3jandrocan0n@gmail.com>
2026-05-21 11:33:53 -0500

    Feature: Working in filters

--- src/components/Recipes/Filters/CustomFilters.tsx
+++ src/components/Recipes/Filters/CustomFilters.tsx
@@ -5,19 +5,89 @@ const CustomFilters = () => {
   return (
     <section className="flex flex-wrap justify-between xl:gap-4 gap-3 md:px-9 xl:px-18">
       <div className="flex flex-col md:flex-row gap-4 w-full md:w-auto">
-        <button className="flex cursor-pointer gap-2 justify-center items-center bg-neutral-0 text-neutral-900 font-medium px-4 py-2.5 rounded-[10px] border-neutral-300 border">
+        <button className="flex relative cursor-pointer gap-2 justify-center items-center bg-neutral-0 text-neutral-900 font-medium px-4 py-2.5 rounded-[10px] border-neutral-300 border">
           Max Prep Time{" "}
           <img
             src={iconDown}
             alt="down"
           />
+          <div className="absolute bg-neutral-0 border-neutral-300 border  translate-y-28.5 rounded-lg p-2 z-10 w-full">
+            <ul className="flex flex-col">
+              <li className="flex gap-2 p-2">
+                <input
+                  type="radio"
+                  name="0-minutes"
+                />
+                <label htmlFor="">0 minutes</label>
+              </li>
+              <li className="flex gap-2 p-2">
+                <input
+                  type="radio"
+                  name="0-minutes"
+                />
+                <label htmlFor="">5 minutes</label>
+              </li>
+              <li className="flex gap-2 p-2">
+                <input
+                  type="radio"
+                  name="0-minutes"
+                />
+                <label htmlFor="">10 minutes</label>
+              </li>
+              <li className="self-start px-2 py-0.5">
+                <button>Clear</button>
+              </li>
+            </ul>
+          </div>
         </button>
-        <button className="flex cursor-pointer gap-2 justify-center items-center bg-neutral-0 text-neutral-900 font-medium px-4 py-2.5 rounded-[10px] border-neutral-300 border">
+        <button className="flex relative cursor-pointer gap-2 justify-center items-center bg-neutral-0 text-neutral-900 font-medium px-4 py-2.5 rounded-[10px] border-neutral-300 border">
           max Cook Time{" "}
           <img
             src={iconDown}
             alt="down"
           />
+          <div className="absolute bg-neutral-0 border-neutral-300 border  translate-y-38.5 rounded-lg p-2 z-10 w-full">
+            <ul className="flex flex-col">
+              <li className="flex gap-2 p-2">
+                <input
+                  type="radio"
+                  name="0-minutes"
+                />
+                <label htmlFor="">0 minutes</label>
+              </li>
+              <li className="flex gap-2 p-2">
+                <input
+                  type="radio"
+                  name="0-minutes"
+                />
+                <label htmlFor="">5 minutes</label>
+              </li>
+              <li className="flex gap-2 p-2">
+                <input
+                  type="radio"
+                  name="0-minutes"
+                />
+                <label htmlFor="">10 minutes</label>
+              </li>
+              <li className="flex gap-2 p-2">
+                <input
+                  type="radio"
+                  name="0-minutes"
+                />
+                <label htmlFor="">15 minutes</label>
+              </li>
+              <li className="flex gap-2 p-2">
+                <input
+                  type="radio"
+                  name="0-minutes"
+                />
+                <label htmlFor="">20 minutes</label>
+              </li>
+              <li className="self-start px-2 py-0.5">
+                <button>Clear</button>
+              </li>
+            </ul>
+          </div>
         </button>
       </div>
       <div className="w-full md:w-77.5 px-4 py-2.5 border-neutral-300 font-medium border relative flex justify-center items-center bg-neutral-0 gap-2 rounded-[10px]">
